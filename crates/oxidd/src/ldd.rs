c3518fd8c77eedeca0d28e30cee95ed851d04487
Maurice Laveaux <m.laveaux@tue.nl>
2026-08-25 16:17:34 +0200

    Increased the cache vector length to 6, required for the predecessors and later the saturation algorithm.

--- crates/oxidd/src/ldd.rs
+++ crates/oxidd/src/ldd.rs
@@ -402,7 +402,7 @@ mod index {
         terminals: 2,
     });
 
-    crate::util::manager_data!(LDDManagerData for LDD, operator: LDDOp, cache_entry_capacity: 5);
+    crate::util::manager_data!(LDDManagerData for LDD, operator: LDDOp, cache_entry_capacity: 6);
 
     crate::util::manager_ref_index_based!(pub struct LDDManagerRef(<LDD as DD>::ManagerRef) with LDDManagerData);
 
@@ -443,7 +443,7 @@ mod pointer {
         tag_bits: 2,
     });
 
-    crate::util::manager_data!(LDDManagerData for LDD, operator: LDDOp, cache_entry_capacity: 5);
+    crate::util::manager_data!(LDDManagerData for LDD, operator: LDDOp, cache_entry_capacity: 6);
 
     crate::util::manager_ref_pointer_based!(pub struct LDDManagerRef(<LDD as DD>::ManagerRef) with LDDManagerData);
 
